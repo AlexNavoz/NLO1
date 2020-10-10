a84a6ad54d2f428cadc19d17c8c24ef07f3de6f4
AlexNavoz <koff.games2020@gmail.com>
2020-10-10 12:46:54 +0300

    engine

---
+++ Assets/Scripts/PressedButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressedButton : MonoBehaviour
+{
+    public bool isPressed = false;
+    
+    public void OnPointerUp()
+    {
+        isPressed = false;
+    }
+
+    public void OnPointerDown()
+    {
+        isPressed = true;
+    }
+}
